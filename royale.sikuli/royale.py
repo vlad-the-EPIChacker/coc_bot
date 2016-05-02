a121f6bbbfbacd1ae6fb3cf814f0f34ff825656d
hi <vladov3000@gmail.com>
2016-05-01 20:17:12 -0700

    s

--- royale.sikuli/royale.py
+++ royale.sikuli/royale.py
@@ -13,11 +13,12 @@ def watch_tv(x):
 def chests():
     c=0
     if exists("1458015332851.png"):
-        c=4
-        click("1458015409623.png")
-    if exists("1457885255132.png"):
         c=5
         click("1458015409623.png")
+    if exists("1457885255132.png"):
+        c=6
+        click("1458015409623.png")
+    time.sleep(1)
     for i in range(0,c):
         click("1457885268951.png")
     if exists("1457885139771.png"):
@@ -26,7 +27,18 @@ def chests():
     for i in range(0,2):
         if exists("1457885556109.png"):
              click("1457885556109.png")
+             time.sleep(1)
              for i in range(0,4):
                   click("1457885268951.png")
 #watch_tv(10)
-chests()
+#click("1458104067967.png")
+#click("1458104105051.png")
+#time.sleep(0.5)
+#type("lol dis is vlad bot")
+#click("1458187324243.png")
+#click("1458187357146.png")
+
+while True:
+    time.sleep(3)
+    click("1458143926796.png")
+    chests()
